be941f9fe1c3a6f74381bbb2c9ff0ea5ddf43c4b
Akkikura <sergon.d.ru@gmail.com>
2024-05-10 16:46:09 +0300

    123

--- phones/views.py
+++ phones/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from phones.models import Phone
 
@@ -9,14 +9,23 @@ def index(request):
 
 def show_catalog(request):
     phones_all = Phone.objects.all()
+    sort_by = request.GET.get('sort', None)
+    if sort_by:
+        if sort_by == 'name':
+            phones_all = phones_all.order_by('name')
+        elif sort_by == 'min_price':
+            phones_all = phones_all.order_by('price')
+        elif sort_by == 'max_price':
+            phones_all = phones_all.order_by('-price')
     template = 'catalog.html'
     context = {'phones': phones_all}
     return render(request, template, context)
 
 
 def show_product(request, slug):
-    phone = Phone.objects.filter(slug__contains=slug).all()
-
+    # Используем get_object_or_404 для получения объекта Phone по slug
+    phone = get_object_or_404(Phone, slug=slug)
     template = 'product.html'
     context = {'phone': phone}
     return render(request, template, context)
+
